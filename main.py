bbcd4c3c4a23a4d93af1f8b1af6d37cebc88169d
4eh711 <Cheh711@gmail.com>
2023-08-28 10:14:24 +0300

    Less_7_Ex2. Цикл для ітерування

--- main.py
+++ main.py
@@ -2,8 +2,5 @@
 my_list=[1,2,3]
 iterator = iter(my_list)
 
-print(next(iterator))
-print(next(iterator))
-print(next(iterator))
-print(next(iterator))
-print(next(iterator))
+for elem in iterator:
+    print(elem)
