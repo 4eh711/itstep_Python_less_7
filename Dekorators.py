c2f43dc9319d565ea9747109c930aa73217bb2a2
4eh711 <Cheh711@gmail.com>
2023-08-28 23:50:48 +0300

    Less_7_Ex7. Декоратори + Замикання

--- Dekorators.py
+++ Dekorators.py
@@ -1,12 +1,15 @@
-def cheker(function, *args, **kwargs):
-    try:
-        result = function(*args, **kwargs)
-    except Exception as exc:
-        print(f"We have problems {exc}")
-    else:
-        print(f"No problems. Result - {result}")
+def cheker(function):
+    def cheker(*args, **kwargs):
+        try:
+            result = function(*args, **kwargs)
+        except Exception as exc:
+            print(f"We have problems {exc}")
+        else:
+            print(f"No problems. Result - {result}")
+    return  cheker
 
 def calculate(expression):
     return eval(expression)
 
-cheker(calculate, "2+2")
+calculate = cheker(calculate)
+calculate("2+2")
