507f2c2ac007436c8b9fe8baf69fa9fe1581ffb7
4eh711 <Cheh711@gmail.com>
2023-08-29 00:01:34 +0300

    Less_7_Ex8. Декоратори + Замикання_3(Додаткове огортання)

--- Dekorators.py
+++ Dekorators.py
@@ -1,14 +1,16 @@
-def cheker(function):
-    def cheker(*args, **kwargs):
-        try:
-            result = function(*args, **kwargs)
-        except Exception as exc:
-            print(f"We have problems {exc}")
-        else:
-            print(f"No problems. Result - {result}")
-    return  cheker
+def checker(*exc_types):
+    def checker(function):
+        def checker(*args, **kwargs):
+            try:
+                result = function(*args, **kwargs)
+            except Exception as exc:
+                print(f"We have problems {exc}")
+            else:
+                print(f"No problems. Result - {result}")
+        return checker
+    return checker
 
-@cheker
+@checker(NameError, TypeError, SyntaxError)
 def calculate(expression):
     return eval(expression)
 
